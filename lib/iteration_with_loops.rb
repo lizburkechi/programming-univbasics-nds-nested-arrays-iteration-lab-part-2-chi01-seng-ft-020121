13cd2a1c0a6bcca6db708b7466b0944097692766
Liz Burke <liz.burke.chi@gmail.com>
2021-01-15 23:07:30 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,7 +7,7 @@ def find_min_in_nested_arrays(src)
 
   while row_index < src.count do
     element_index = 0
-    min_in_nested_array = []
+    min_in_nested_array = [100]
 
     while element_index < src[row_index].count do
 
