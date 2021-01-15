b152b49f9a35fb26e602926a8d2683c62dee75b2
Liz Burke <liz.burke.chi@gmail.com>
2021-01-15 22:11:43 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -13,17 +13,17 @@ array_of_daily_temperatures = [
 def find_min_in_nested_arrays(src)
   outer_results = []
   row_index = 0
-  while row_index < spice_rack.count do
+  while row_index < src.count do
     element_index = 0
-    longest_string_element = ""
-    while element_index < spice_rack[row_index].count do
+    min_in_nested_array = ""
+    while element_index < array_of_daily_temperatures[row_index].count do
 
       if spice_rack[row_index][element_index].length > longest_string_element.length
         longest_string_element = spice_rack[row_index][element_index]
       end
       element_index += 1
     end
-    outer_results << longest_string_element
+    outer_results << min_in_nested_array
  row_index += 1
 end
 
