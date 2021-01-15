d39fad669646dad31db5ac3101fb2412351c59b5
Liz Burke <liz.burke.chi@gmail.com>
2021-01-15 22:23:29 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -15,16 +15,20 @@ def find_min_in_nested_arrays(src)
   row_index = 0
   while row_index < src.count do
     element_index = 0
-    min_in_nested_array = ""
+    min_in_nested_array = []
 
     while element_index < array_of_daily_temperatures[row_index].count do
+      
       if array_of_daily_temperatures[row_index][element_index].count > min_in_nested_array.count
         min_in_nested_array = array_of_daily_temperatures[row_index][element_index]
+        
       end
       element_index += 1
+      
     end
     outer_results << min_in_nested_array
  row_index += 1
+ 
 end
 
 
