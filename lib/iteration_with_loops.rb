089e84076e7d8ac945dcf237d304745e36802308
Liz Burke <liz.burke.chi@gmail.com>
2021-01-15 22:28:53 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -13,6 +13,7 @@ array_of_daily_temperatures = [
 def find_min_in_nested_arrays(src)
   outer_results = []
   row_index = 0
+  
   while row_index < src.count do
     element_index = 0
     min_in_nested_array = []
